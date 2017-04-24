affc3e9f0a87393ca6fad5278baa7074bf63aa73
Aline <alinechribeiro@gmail.com>
2017-04-24 20:57:25 +0100

    work it better

--- threads/templatetags/threads_extras.py
+++ threads/templatetags/threads_extras.py
@@ -32,13 +32,13 @@ def user_vote_button(thread, subject, user):
 
     if not vote:
         if user.is_authenticated():
-            link = """
+            link = 
                 <div class="col-md-3 btn-vote">
                     <a href="%s" class="btn btn-default btn-sm">
                         Add my vote!
                     </a>
                 </div>
-            """ % reverse('cast_vote', kwargs={
+             % reverse('cast_vote', kwargs={
                 'thread_id': thread.id,
                 'subject_id': subject.id
             })
