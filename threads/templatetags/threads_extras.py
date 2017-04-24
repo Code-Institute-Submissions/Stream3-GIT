a78ba59e64f39793b972baaae5f05d7d95d931c2
Aline <alinechribeiro@gmail.com>
2017-04-24 21:08:21 +0100

    work it better

--- threads/templatetags/threads_extras.py
+++ threads/templatetags/threads_extras.py
@@ -28,23 +28,20 @@ def last_posted_user_name(thread):
 
 @register.simple_tag
 def user_vote_button(thread, subject, user):
-    vote = thread.poll.votes.filter(user_id=user.id).first()
-
-    if not vote:
-        if user.is_authenticated():
-            link = 
-                <div class="col-md-3 btn-vote">
-                    <a href="%s" class="btn btn-default btn-sm">
-                        Add my vote!
-                    </a>
-                </div>
-             % reverse('cast_vote', kwargs={
-                'thread_id': thread.id,
-                'subject_id': subject.id
-            })
-
-            return link
-    return ""
+   vote = thread.poll.votes.filter(user_id=user.id).first()
+ 
+   if not vote:
+       if user.is_authenticated():
+           link = """
+           <div class="col-md-3 btn-vote"> 
+           <a href="%s" class="btn btn-default btn-sm">
+             Add my vote!
+           </a>
+           </div>""" % reverse('cast_vote', kwargs={'thread_id' : thread.id, 'subject_id':subject.id})
+ 
+           return link
+ 
+   return ""
 
 
 @register.filter
