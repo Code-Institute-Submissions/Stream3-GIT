0e7876ab8e1f1ee6d47cd8278ac44bb0bcbfd280
Aline <alinechribeiro@gmail.com>
2017-04-24 16:21:38 +0100

    work it better

--- account/models.py
+++ account/models.py
@@ -11,9 +11,7 @@ class UserProfile(models.Model):
   #Other fields here
   name = models.CharField(max_length=50)
   address = models.CharField(max_length=50)
-  #can also create a field for User image:
-  image = models.ImageField(upload_to="images", blank=True, null=True)
-
+ 
 class AccountUserManager(UserManager):
     def _create_user(self, username, email, password,
                      is_staff, is_superuser, **extra_fields):
