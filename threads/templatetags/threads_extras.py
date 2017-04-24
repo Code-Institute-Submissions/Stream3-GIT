0ff3942ff558c3397fe93234ab0bbe22bc94f51e
Aline <alinechribeiro@gmail.com>
2017-04-24 20:54:11 +0100

    work it better

--- threads/templatetags/threads_extras.py
+++ threads/templatetags/threads_extras.py
@@ -4,7 +4,6 @@ from django.core.urlresolvers import reverse
 
 register = template.Library()
 
-
 @register.filter
 def get_total_subject_posts(subject):
     total_posts = 0
@@ -17,20 +16,16 @@ def get_total_subject_posts(subject):
 def started_time(created_at):
     return arrow.get(created_at).humanize()
 
+
 @register.simple_tag
 def last_posted_user_name(thread):
     posts = thread.posts.all().order_by('-created_at')
-    return posts[posts.count()-1].user.username
-# @register.simple_tag
-# def last_posted_user_name(thread):
-#     posts = thread.posts.all().order_by('-created_at')
-#     if posts.count() > 0:
-#         return posts[posts.count() - 1].user.first_name
-#     else:
-#         return "Nothing Posted"
+    if posts.count() > 0:
+        return posts[posts.count() - 1].user.first_name
+    else:
+        return "Nothing Posted"
 
 
-# When we render, we hide the voting buttons from our view. 
 @register.simple_tag
 def user_vote_button(thread, subject, user):
     vote = thread.poll.votes.filter(user_id=user.id).first()
