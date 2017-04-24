65754318a3395086af20b3360e22e7ba7460b92d
Aline <alinechribeiro@gmail.com>
2017-04-24 23:47:04 +0100

    work it better

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -149,20 +149,19 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 ############ FOR PRODUCTION
-"""
+
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
     os.path.join(PROJECT_ROOT, 'static'),
 )
-"""
 #####################################################
 
 ############ FOR DEV
-STATICFILES_DIRS = (
+"""STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-)
+)"""
 #####################################################
 
 
